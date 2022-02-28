13a653726432585807689db99c9f321546f2f9ec
Dustin Smith <dustinsmith1983@gmail.com>
2022-02-27 22:14:34 -0800

    completed store.js

--- db/store.js
+++ db/store.js
@@ -1,7 +1,7 @@
 const util = require('util');
 const fs = require('fs');
-
 const uuidv1 = require('uuid/v1');
+
 
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
@@ -18,16 +18,35 @@ class Store {
     getNotes() {
         return this.read().then((notes) => {
             let parsedNotes;
-
             try {
                 parsedNotes = [].concat(JSON.parse(notes));
             } catch(err) {
                 parsedNotes = [];
             }
-
             return parsedNotes;
         });
     }
 
-    
+    addNote(note) {
+        const { title, text} = note;
+
+        if (!title || !text) {
+            throw new Error("Please enter a valid title and note, they cannot be blank");
+        }
+
+        const newNote = { title, text, id: uuidv1() };
+
+        return this.getNotes()
+        .then((notes) => [...notes,newNote])
+        .then((updatedNotes) => this.write(updatedNotes))
+        .then(() => newNote);
+    }
+
+    removeNote(id) {
+        return this.getNotes()
+        .then((notes) => notes.filter((note) => note.id !== id))
+        .then((filteredNotes) => this.write(filteredNotes));
+    }
 }
+
+module.exports = new Store();
